66ecbfb78700b467d3e69ed6d3a11455c8fd6612
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2022-09-02 09:14:16 -0400

    Create c10ecollector.sh

---
+++ c10ecollector.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GATEWAY_ADDRESS={YOUR-COMPANY}.chronosphere.io:443 
+export API_TOKEN=xxxxxxxxxxxxxxxxxxxxxxx 
+nohup /home/ec2-user/c10e/chronocollector-linux-amd64 -f /home/ec2-user/c10e/c10ecollector-config.yml &
