64a3827f7736f500626e3b1030c311db265edd82
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2022-09-05 13:52:07 -0400

    Update yace.sh

--- yace.sh
+++ yace.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 5000:5000 -v /home/ec2-user/yace/conf/cw-cred:/exporter/.aws/credentials -v /home/ec2-user/yace/conf/config.yml:/tmp/yace-config.yml quay.io/invisionag/yet-another-cloudwatch-exporter:v0.28.0-alpha
+docker run -d -p 5000:5000  -v /home/ec2-user/yace/conf/cw-cred:/exporter/.aws/credentials -v /home/ec2-user/yace/conf/config-externalid.yml:/tmp/config.yml duplocloud/yace:205f790f6e97b966591000c11ec35be70e349bc0
